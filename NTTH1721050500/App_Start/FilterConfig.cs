2c0837971310bcfe2c5ac88a508a9b07d02a3472
ThuyHang3007 <89243263+ThuyHang3007@users.noreply.github.com>
2021-10-26 13:31:25 +0700

    Add project files.

---
+++ NTTH1721050500/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace NTTH1721050500
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ NTTH1721050500/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NTTH1721050500.MvcApplication" Language="C#" %>
